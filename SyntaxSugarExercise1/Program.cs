7d1a9aa87e07302dea6a02774733b3688c8b969e
Sokpagna Lim <sokpagnalim@gmail.com>
2022-06-03 09:33:32 -0700

    SyntaxSugarExercise

---
+++ SyntaxSugarExercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SyntaxSugarExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Given the following code, change the code to implement:
+            //inferred typing
+            //string interpolation
+            //and the ternary operator
+            //int answer=4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+
+         
+            var answer =  4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer}is greater than nine";
+            Console.WriteLine(response);
+            
+        }
+    }
+}
